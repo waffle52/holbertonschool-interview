79547cb9c0ce3f1d960cc4e1a4d138c504d029b9
waffle52 <asuka9767@protonmail.com>
2020-12-10 17:07:34 -0800

    Redid program to just add keys directly and used the built in enumerate function to get all the keys from boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,22 +5,15 @@ from gathering keys from other boxes
 """
 
 
-def connect(T, R):
-    res = []
-    for x in R:
-        res += T[x]
-    return res
-
-
 def canUnlockAll(boxes):
-    index = 0
-    total = list(set(boxes[0]) | {0})
-    added = True
-    while added:
-        added = False
-        for i in connect(boxes, total[index:]):
-            if i not in total:
-                total.append(i)
-                index += 1
-                added = True
-    return (len(total) == len(boxes))
+    open = {0}
+    # goes through adding every key
+    for i, box in enumerate(boxes):
+        for key in box:
+            if key != i:
+                open.add(key)
+    for i in range(len(boxes)):
+        # checks to see if every box number is located in open
+        if i not in open:
+            return False
+    return True
