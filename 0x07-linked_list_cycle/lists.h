7b0f7b0e182163ac900e16a0b8d3efe4cc4b4b6a
waffle52 <asuka9767@protonmail.com>
2021-02-26 18:15:49 -0800

    Fixed typos in header file

--- 0x07-linked_list_cycle/lists.h
+++ 0x07-linked_list_cycle/lists.h
@@ -6,15 +6,15 @@
 /**
  * struct listint_s - singly linked list
  * @n: integer
- * @Next: points to the next node
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
